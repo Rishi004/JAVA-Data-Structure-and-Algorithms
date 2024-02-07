d0978d0d1bdb8d646c2083f59451e4ab4f4c3190
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:13:38 +0530

    Arrays

--- 19-Create-Target-Array.java
+++ 19-Create-Target-Array.java
@@ -1,10 +1,10 @@
+// (1389)
 // Given two arrays of integers nums and index. Your task is to create target array under the following rules:
 //   Initially target array is empty.
 //   From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
 //   Repeat the previous step until there are no elements to read in nums and index.
 // Return the target array.
 // It is guaranteed that the insertion operations will be valid.
-// LEETCODE -> 1389. Create Target Array in the Given Order
 
 // Example 1:
   // Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
