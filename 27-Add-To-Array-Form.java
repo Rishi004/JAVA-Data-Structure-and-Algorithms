13da401217002cf0e9544411000c747cd7990bd7
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:20:29 +0530

    Arrays

--- 27-Add-To-Array-Form.java
+++ 27-Add-To-Array-Form.java
@@ -1,7 +1,7 @@
+// (989)
 // The array-form of an integer num is an array representing its digits in left to right order.
   // For example, for num = 1321, the array form is [1,3,2,1].
 // Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
-// LEETCODE -> 989. Add to Array-Form of Integer
 
 // Example 1:
   // Input: num = [1,2,0,0], k = 34
