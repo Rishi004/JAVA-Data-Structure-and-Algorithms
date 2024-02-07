fdaf6b9497b11bcc2ea68d65ed0b550fa7582f31
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:08:03 +0530

    Arrays

--- 11-Get-Concatenation.java
+++ 11-Get-Concatenation.java
@@ -1,7 +1,7 @@
 // Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 // Specifically, ans is the concatenation of two nums arrays.
 // Return the array ans.
-// LEETCODE -> 1929. Concatenation of Array
+// (1929)
 
 // Example 1:
   // Input: nums = [1,2,1]
