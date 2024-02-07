003cb5327521c2cf43e526286c6ae0eb3868668a
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:12:24 +0530

    Arrays

--- 16-Num-Identical-Pairs.java
+++ 16-Num-Identical-Pairs.java
@@ -1,6 +1,6 @@
+// (1512)
 // Given an array of integers nums, return the number of good pairs.
 // A pair (i, j) is called good if nums[i] == nums[j] and i < j.
-// LEETCODE -> 1512. Number of Good Pairs
 
 // Example 1:
   // Input: nums = [1,2,3,1,1,3]
