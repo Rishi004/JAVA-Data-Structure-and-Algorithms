2a885b119708e0d36c4eb75b43f60a0a628159f5
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:11:06 +0530

    Arrays

--- 13-Maximum-Wealth.java
+++ 13-Maximum-Wealth.java
@@ -1,6 +1,6 @@
+// (1672)
 // You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
 // A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
-// LEETCODE -> 1672. Richest Customer Wealth
 
 // Example 1:
   // Input: accounts = [[1,2,3],[3,2,1]]
