d7af7bbda524dbb8aa86434f182503c2fafd6b00
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:20:07 +0530

    Arrays

--- 26-Transpose-Matrix.java
+++ 26-Transpose-Matrix.java
@@ -1,6 +1,6 @@
+// (867)
 // Given a 2D integer array matrix, return the transpose of matrix.
 // The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
-// LEETCODE -> 867. Transpose Matrix
 
 // Example 1:
   // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
