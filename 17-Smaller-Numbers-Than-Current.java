9f43190c502fd7b3afca3db89f7076bca25ce625
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:12:53 +0530

    Arrays

--- 17-Smaller-Numbers-Than-Current.java
+++ 17-Smaller-Numbers-Than-Current.java
@@ -1,6 +1,6 @@
+// (1365)
 // Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
 // Return the answer in an array.
-// LEETCODE -> 1365. How Many Numbers Are Smaller Than the Current Number
 
 // Example 1:
   // Input: nums = [8,1,2,2,3]
