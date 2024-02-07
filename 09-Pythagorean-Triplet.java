cdfbe67c181035c79ba49b5e60f1c59053e6b042
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:06:58 +0530

    Update 09-Pythagorean-Triplet.java

--- 09-Pythagorean-Triplet.java
+++ 09-Pythagorean-Triplet.java
@@ -1,6 +1,5 @@
 // Pythagorean Triplet in an array.
 // Given an array of integers, write a function that returns true if there is a triplet (a, b, c) that satisfies a2 + b2 = c2.
-// GEEKSFORGEEKS -> https://www.geeksforgeeks.org/find-pythagorean-triplet-in-an-unsorted-array/
 
 // Example 1:
   // Input: arr[] = {3, 1, 4, 6, 5} 
