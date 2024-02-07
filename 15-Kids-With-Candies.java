f53236def93988669c9188f8170f8bfa02ecd398
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:11:56 +0530

    Arrays

--- 15-Kids-With-Candies.java
+++ 15-Kids-With-Candies.java
@@ -1,7 +1,7 @@
+// (1431)
 // There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 // Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
 // Note that multiple kids can have the greatest number of candies.
-// LEETCODE -> 1431. Kids With the Greatest Number of Candies
 
 // Example 1:
 //   Input: candies = [2,3,5,1,3], extraCandies = 3
