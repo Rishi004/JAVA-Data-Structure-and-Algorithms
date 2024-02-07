aad397ea07c60ff79e3bda30e084ba397a5683a8
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:11:35 +0530

    Arrays

--- 14-Shuffle-Array.java
+++ 14-Shuffle-Array.java
@@ -1,6 +1,6 @@
+// (1470)
 // Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 // Return the array in the form [x1,y1,x2,y2,...,xn,yn].
-// LEETCODE -> 1470. Shuffle the Array
 
 // Example 1:
 //   Input: nums = [2,5,1,3,4,7], n = 3
