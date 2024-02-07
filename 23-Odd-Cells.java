f21e3b95754039902f4be20b2682baf102af1ff2
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:18:28 +0530

    Arrays

--- 23-Odd-Cells.java
+++ 23-Odd-Cells.java
@@ -1,9 +1,9 @@
+// (1252)
 // There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
 // For each location indices[i], do both of the following:
   // 1. Increment all the cells on row ri.
   // 2. Increment all the cells on column ci.
 // Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
-//LEETCODE -> 1252. Cells with Odd Values in a Matrix
 
 // Example 1:
   // Input: m = 2, n = 3, indices = [[0,1],[1,1]]
