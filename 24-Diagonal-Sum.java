d9b93cc4e28fb1d4a86c8e49ee0d5b525e1e243f
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-07 16:18:56 +0530

    Arrays

--- 24-Diagonal-Sum.java
+++ 24-Diagonal-Sum.java
@@ -1,6 +1,6 @@
+// (1572)
 // Given a square matrix mat, return the sum of the matrix diagonals.
 // Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
-// LEETCODE -> 1572. Matrix Diagonal Sum
 
 // Example 1:
   // Input: mat = [[1,2,3],
