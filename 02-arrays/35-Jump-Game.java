b991fe6f0da6810ad1e2c8c6c07a620e0989aaaf
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-14 12:39:41 +0530

    arrays

--- 02-arrays/35-Jump-Game.java
+++ 02-arrays/35-Jump-Game.java
@@ -16,3 +16,22 @@
   // 1 <= nums.length <= 104
   // 0 <= nums[i] <= 105
 
+package com.rishi;
+
+public class JumpGame {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(canJump(nums));
+    }
+
+    static boolean canJump(int[] nums) {
+        int len = nums.length - 1, target = nums.length - 1;
+        for (int i = len -1; i >= 0; i--) {
+            if (i + nums[i] >= target) {
+                target = i;
+            }
+        }
+        return target == 0;
+    }
+}
+
