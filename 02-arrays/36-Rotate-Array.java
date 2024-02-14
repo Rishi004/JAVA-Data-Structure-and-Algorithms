285a7a864257fa9d9e55dc78dd08b304db839a46
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-14 13:34:29 +0530

    arrays

--- 02-arrays/36-Rotate-Array.java
+++ 02-arrays/36-Rotate-Array.java
@@ -16,3 +16,62 @@
   // rotate 1 steps to the right: [99,-1,-100,3]
   // rotate 2 steps to the right: [3,99,-1,-100]
 
+
+// Way 1 - Basic solution
+package com.rishi;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k);
+    }
+
+    static void rotate(int[] nums, int k) {
+        int len = nums.length - 1;
+        for (int i = 1; i <= k; i++) {
+            int last = nums[len];
+            for (int j = len; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = last;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
+
+// Way 2 - Optimized solution
+package com.rishi;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotate(nums, k);
+    }
+
+    static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void reverse (int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
