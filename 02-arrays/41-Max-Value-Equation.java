5a20c642370057de09fae59e0a55d5cb5e809412
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-02-20 14:35:11 +0530

    arrays

--- 02-arrays/41-Max-Value-Equation.java
+++ 02-arrays/41-Max-Value-Equation.java
@@ -21,16 +21,13 @@ package com.rishi;
 public class MaxValueOfEquation {
     public static void main(String[] args) {
         int[][] points = {
-                {1, 3}, {2, 0}, {5, 10}, {6, -10}
+               {-19, -12}, {-13, -18}, {-12, 18}, {-11, -8},
+               {-8, 2}, {-7, 12}, {-5, 16}, {-3, 9}, {1, -7},
+               {5, -4}, {6, -20}, {10, 4}, {16, 4}, {19, -9},
+               {20, 19}
         };
-//        int[][] points = {
-//                {-19, -12}, {-13, -18}, {-12, 18}, {-11, -8},
-//                {-8, 2}, {-7, 12}, {-5, 16}, {-3, 9}, {1, -7},
-//                {5, -4}, {6, -20}, {10, 4}, {16, 4}, {19, -9},
-//                {20, 19}
-//        };
-        int k = 1;
-        System.out.println(findMaxValueOfEquation(points, k));
+        int k = 6;
+        System.out.println(findMaxValueOfEquation(points, k)); // Output: 35
     }
 
     static int findMaxValueOfEquation(int[][] points, int k) {
