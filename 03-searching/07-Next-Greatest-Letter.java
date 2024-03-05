28f5b701fc8c3e15a2a1b85e6a789f71414008c4
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-03-05 10:55:01 +0530

    binary search

--- 03-searching/07-Next-Greatest-Letter.java
+++ 03-searching/07-Next-Greatest-Letter.java
@@ -3,6 +3,7 @@
 // There are at least two different characters in letters.
 // Return the smallest character in letters that is lexicographically greater than target.
 // If such a character does not exist, return the first character in letters.
+// You must write an algorithm with O(log n) runtime complexity.
 
 // Example 1:
   // Input: letters = ["c","f","j"], target = "a"
