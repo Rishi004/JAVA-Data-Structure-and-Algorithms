9193775f1962cc68d1d7af849bd43bbce4f3dda0
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-03-05 07:44:19 +0530

    binary search

--- 03-searching/03-Square-Root.java
+++ 03-searching/03-Square-Root.java
@@ -1,3 +1,4 @@
+// (69)
 // Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
 // You must not use any built-in exponent function or operator.
 // For example, do not use Math.pow(x, 05)
