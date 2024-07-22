294f67e049bc3e0561337356544750c481fc895a
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-07-22 11:21:40 +0530

    find minimum in rotated sorted array

---
+++ 03-searching/29-find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,54 @@
+// (153)
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+  // [4,5,6,7,0,1,2] if it was rotated 4 times.
+  // [0,1,2,4,5,6,7] if it was rotated 7 times.
+  
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+// You must write an algorithm that runs in O(log n) time.
+
+// Example 1:
+  // Input: nums = [3,4,5,1,2]
+  // Output: 1
+  // Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+  
+// Example 2:
+  // Input: nums = [4,5,6,7,0,1,2]
+  // Output: 0
+  // Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+  
+// Example 3:
+  // Input: nums = [11,13,15,17]
+  // Output: 11
+  // Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+
+
+package com.rishi.searching;
+
+public class FindMin {
+    public static void main(String[] args) {
+        int[] nums = {11,13,15,17};
+        System.out.println(findMin(nums));
+    }
+
+    static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            } else if (start < mid && nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            } else if (nums[mid] <= nums[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[0];
+    }
+}
