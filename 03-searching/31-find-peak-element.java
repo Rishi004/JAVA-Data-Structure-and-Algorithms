228aea01fc1613bd8fb988533979debd60123615
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-07-22 09:07:58 +0530

    find peak element

---
+++ 03-searching/31-find-peak-element.java
@@ -0,0 +1,41 @@
+// (162)
+// A peak element is an element that is strictly greater than its neighbors.
+// Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+// You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+// You must write an algorithm that runs in O(log n) time.
+
+// Example 1:
+  // Input: nums = [1,2,3,1]
+  // Output: 2
+  // Explanation: 3 is a peak element and your function should return the index number 2.
+  
+// Example 2:
+  // Input: nums = [1,2,1,3,5,6,4]
+  // Output: 5
+  // Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+
+package com.rishi.searching;
+
+public class FindPeakElement {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1, 3, 5, 6, 4};
+        System.out.println(findPeakElement(nums));
+    }
+
+    static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return start;
+    }
+}
