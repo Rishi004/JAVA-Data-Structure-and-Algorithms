f2872cf0c9e79e06a2bcef4809bd283e316aee8d
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-10-07 13:11:10 +0530

    missing number

---
+++ 04-sorting/05-missing-number.java
@@ -0,0 +1,60 @@
+// (268)
+// (Amazon question)
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+// Example 1:
+  // Input: nums = [3,0,1]
+  // Output: 2
+  // Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+  
+// Example 2:
+  // Input: nums = [0,1]
+  // Output: 2
+  // Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+  
+// Example 3:
+  // Input: nums = [9,6,4,2,3,5,7,0,1]
+  // Output: 8
+  // Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+
+package com.rishi.sorting;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1};
+        System.out.println(missingNumber(nums));
+    }
+
+    private static int missingNumber(int[] nums) {
+        cyclicSort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums[i]) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+
+    private static void cyclicSort(int[] arr) {
+        int i = 0;
+        int len = arr.length;
+        while (i < len) {
+            int correctInd = arr[i];
+            if (correctInd < 0 || correctInd >= len || arr[i] == arr[correctInd]) {
+                i++;
+            } else {
+                swap(arr, i, correctInd);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int num1, int num2) {
+        int temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+    }
+}
