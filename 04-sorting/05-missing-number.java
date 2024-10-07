e33bec08461422319aa21242d304e7aaea7a6657
Rishikopan <63440542+Rishi004@users.noreply.github.com>
2024-10-07 14:38:14 +0530

    missing number

--- 04-sorting/05-missing-number.java
+++ 04-sorting/05-missing-number.java
@@ -1,5 +1,5 @@
 // (268)
-// (Amazon question)
+// (Asked in Amazon)
 // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 
 // Example 1:
