d7ff61c2c9518b199fe8030797b485454dae7185
BugguSlayā Rishī <63440542+Rishi004@users.noreply.github.com>
2025-06-11 15:30:08 +0530

    Update 15-string-compression.java

--- 01-basics/15-string-compression.java
+++ 01-basics/15-string-compression.java
@@ -4,6 +4,53 @@
 // You can assume the string has only uppercase and lowercase letters (a-z).
 
 
+// ------------------------------- OPTIMIZED SOLUTION -------------------------------
+public class StringCompression {
+
+    public static void main(String[] args) {
+        String str = "aabcccccaaa";
+        System.out.println(stringCompression(str));
+    }
+
+    private static String stringCompression(String str) {
+
+        int compressedLength = countCompressedLength(str);
+        if (compressedLength > str.length()) {
+            return str;
+        }
+
+        int count = 0;
+        StringBuilder output = new StringBuilder(compressedLength);
+
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                output.append(str.charAt(i)).append(count);
+                count = 0;
+            }
+        }
+        return output.toString();
+    }
+
+    private static int countCompressedLength(String str) {
+        int compressedLength = 0;
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+
+            if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+                compressedLength += 1 + String.valueOf(count).length();
+                count = 0;
+            }
+        }
+
+        return compressedLength;
+    }
+}
+
+
+// ------------------------------- BRUTE FORCE SOLUTION -------------------------------
 public class StringCompression {
 
     public static void main(String[] args) {
